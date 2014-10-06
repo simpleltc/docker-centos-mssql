844f5ab22da6d10cb597e509dd847e112b042015
Mark Adams <mark@simpleltc.com>
2014-10-06 15:33:08 -0500

    Fixed a broken download link for the SQL Server ODBC driver (fixing #1) and set the proper compilation flags for unixODBC

--- setup_mssql.sh
+++ setup_mssql.sh
@@ -17,7 +17,10 @@ tar -xvf unixODBC-2.3.2.tar.gz
 cd unixODBC-2.3.2
 
 # Build unixODBC to /usr/lib64
-./configure --libdir=/usr/lib64
+CPPFLAGS="-DSIZEOF_LONG_INT=8"
+export CPPFLAGS
+
+./configure --prefix=/usr --libdir=/usr/lib64 --sysconfdir=/etc --enable-gui=no --enable-drivers=no --enable-iconv --with-iconv-char-enc=UTF8
 make
 make install
 
@@ -33,18 +36,19 @@ ln -s /usr/lib64/libodbc.so.2.0.0 /usr/lib64/libodbc.so.1
 cd $BASE
 
 # Download the MS SQL ODBC Driver for Linux from Microsoft
-if ! wget http://download.microsoft.com/download/6/A/B/6AB27E13-46AE-4CE9-AFFD-406367CADC1D/Linux6/sqlncli-11.0.1790.0.tar.gz; then
+if ! wget http://download.microsoft.com/download/B/C/D/BCDD264C-7517-4B7D-8159-C99FC5535680/RedHat6/msodbcsql-11.0.2270.0.tar.gz; then
     echo 'Failed to download MS SQL ODBC driver' 1>&2
     exit 1
 fi
 
 # Extract the MS SQL ODBC Driver
 mkdir mssql
-tar -xvf sqlncli-11.0.1790.0.tar.gz
-cd sqlncli-11.0.1790.0
+tar -xvf msodbcsql-11.0.2270.0.tar.gz
+cd msodbcsql-11.0.2270.0
 
 # Modify the install script to work with unixODBC 2.3.2
 sed -i -e 's/req_dm_ver="2.3.0";/req_dm_ver="2.3.2";/' install.sh
+
 
 # Install the MS SQL ODBC driver
 if ! ./install.sh install --accept-license; then
